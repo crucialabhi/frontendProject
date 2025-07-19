9123ee564fe2474870c831ad38175cbcb2d8f0b1
crucialabhi <abhishekmishra445577@gmail.com>
2025-07-19 15:37:16 +0530

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-800'>
+    hey
+    </div>
+    </>
+  )
+}
+
+export default App
